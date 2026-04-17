1e22e8af139391cfd705b4942efe421e5ae81057
ankitoid <ankitsinghghoshi1234@gmail.com>
2026-04-17 12:54:51 +0530

    new container changes

--- src/components/common/container.tsx
+++ src/components/common/container.tsx
@@ -1,22 +1,22 @@
 import React, { forwardRef } from "react";
 
 interface ContainerProps {
-    children: React.ReactNode;
-    className?: string;
+  children: React.ReactNode;
+  className?: string;
 }
 
 const Container = forwardRef<HTMLDivElement, ContainerProps>(
-    ({ children, className, ...props }, ref) => {
-        return (
-            <div
-                ref={ref}
-                className={`animate-fade-in-blur container mx-auto 2xl:max-w-400 md:max-w-280 max-w-95 px-1 py-0 ${className}`}
-                {...props}
-            >
-                {children}
-            </div>
-        );
-    },
+  ({ children, className, ...props }, ref) => {
+    return (
+      <div
+        ref={ref}
+        className={`animate-fade-in-blur container mx-auto max-w-7xl px-4 ${className}`}
+        {...props}
+      >
+        {children}
+      </div>
+    );
+  },
 );
 
 Container.displayName = "Container";
